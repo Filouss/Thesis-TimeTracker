54b4ff66e94bfb4609b55b8db38f0bc492360f36
sarovfil <sarovfil@fel.cvut.cz>
2026-04-11 10:51:31 +0200

    tweaked axios settings

--- GitHub-timetracker/react-app/src/lib/http.ts
+++ GitHub-timetracker/react-app/src/lib/http.ts
@@ -1,5 +1,7 @@
 import axios from "axios";
 
+axios.defaults.withCredentials = true;
+
 export const http = axios.create({
     baseURL: import.meta.env.VITE_API_BASE_URL ?? "/api",
     withCredentials: true, 
